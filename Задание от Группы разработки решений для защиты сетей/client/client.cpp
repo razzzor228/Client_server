7bb4057ad16df1262f978a960d2cc1421813e4f5
razzzor228 <110452473+razzzor228@users.noreply.github.com>
2023-11-24 18:11:37 +0300

    Add files via upload

---
+++ Задание от Группы разработки решений для защиты сетей/client/client.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    // Инициализация WinSock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Ошибка при инициализации WinSock." << std::endl;
+        return 1;
+    }
+
+    // Создание сокета
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Ошибка при создании сокета." << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // Ввод адреса и порта сервера
+    std::cout << "Введите IP-адрес сервера: ";
+    std::string serverAddress;
+    std::cin >> serverAddress;
+    std::cout << "Введите порт сервера: ";
+    unsigned short port;
+    std::cin >> port;
+
+    // Установка соединения с сервером
+    sockaddr_in serverSocketAddress{};
+    serverSocketAddress.sin_family = AF_INET;
+    inet_pton(AF_INET, serverAddress.c_str(), &(serverSocketAddress.sin_addr));
+    serverSocketAddress.sin_port = htons(port);
+    if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverSocketAddress), sizeof(serverSocketAddress)) ==
+        SOCKET_ERROR) {
+        std::cerr << "Ошибка при установке соединения с сервером." << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Соединение с сервером установлено. Ожидание данных..." << std::endl;
+
+    // Получение данных от сервера
+    char buffer[1024];
+    memset(buffer, 0, sizeof(buffer));
+    if (recv(clientSocket, buffer, sizeof(buffer) - 1, 0) == SOCKET_ERROR) {
+        std::cerr << "Ошибка при получении данных от сервера." << std::endl;
+    }
+    else {
+        std::cout << "Получены данные от сервера: " << buffer << std::endl;
+    }
+
+    // Закрытие соединения и освобождение ресурсов
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
+}
---
+++ Задание от Группы разработки решений для защиты сетей/server/server.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <string>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    // Инициализация WinSock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Ошибка при инициализации WinSock." << std::endl;
+        return 1;
+    }
+
+    // Создание сокета
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cerr << "Ошибка при создании сокета." << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // Ввод прослушивающего порта
+    std::cout << "Введите прослушивающий порт: ";
+    unsigned short port;
+    std::cin >> port;
+
+    // Привязка сокета к адресу и порту
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(port);
+    if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
+        std::cerr << "Ошибка при привязке сокета к адресу и порту." << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // Прослушивание входящих соединений
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cerr << "Ошибка при прослушивании входящих соединений." << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Сервер ожидает соединения на порту " << port << "..." << std::endl;
+
+    // Принятие входящего соединения
+    SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Ошибка при принятии входящего соединения." << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Соединение установлено. Отправка данных клиенту..." << std::endl;
+
+    // Отправка данных клиенту
+    std::string message = "Hello Maxim!";
+    if (send(clientSocket, message.c_str(), message.length(), 0) == SOCKET_ERROR) {
+        std::cerr << "Ошибка при отправке данных клиенту." << std::endl;
+    }
+
+    // Закрытие соединения и освобождение ресурсов
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+
+    std::cout << "Соединение закрыто. Сервер завершает работу." << std::endl;
+
+    return 0;
+}
